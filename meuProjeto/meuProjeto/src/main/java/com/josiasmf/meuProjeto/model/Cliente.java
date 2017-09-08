ff6fa83ca19f65e925d6dc5a51eba12c31d92ab5
Josias <jomattiole@gmail.com>
2017-09-07 22:14:22 -0300

    land page
    Criação da landpage do projeto

---
+++ meuProjeto/meuProjeto/src/main/java/com/josiasmf/meuProjeto/model/Cliente.java
@@ -0,0 +1,59 @@
+package com.josiasmf.meuProjeto.model;
+
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "CLIENTE")
+public class Cliente implements Serializable{
+    
+    @Id
+    @Column(name = "ID")
+    @NotNull
+    private Long id;
+    
+    @Column(name = "NOME")
+    @NotNull
+    private String nome;
+    
+    @Column(name = "EMAIL")
+    @NotNull
+    private String email;
+
+    public Cliente() {
+    }
+
+    public Cliente(Long id, String nome, String email) {
+        this.id = id;
+        this.nome = nome;
+        this.email = email;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+    
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
---
+++ meuProjeto/meuProjeto/src/main/java/com/josiasmf/meuProjeto/util/ClienteRepository.java
@@ -0,0 +1,50 @@
+package com.josiasmf.meuProjeto.util;
+
+import com.josiasmf.meuProjeto.model.Cliente;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ClienteRepository {
+    
+    private final List<Cliente> clientes = new ArrayList<>();
+    private long nextId = 0;
+    
+    
+    public List<Cliente> findAll() {
+        return clientes;
+    }
+    
+    public Cliente findById(long id) {
+        for (Cliente cliente : clientes) {
+            if (cliente.getId().equals(id))
+                return cliente;
+        }
+        return null;
+    }
+    
+    public void insert(Cliente cliente) {
+        cliente.setId(++nextId);
+        clientes.add(cliente);
+    }
+    
+    public void update(long id, Cliente cliente) {
+        for (int i = 0; i < clientes.size(); i++) {
+            if (clientes.get(i).getId().equals(id)) {
+                clientes.set(i, cliente);
+                return;
+            }
+        }
+    }
+    
+    public void delete(long id) {
+        Cliente selecionado = null;
+        for (int i = 0; i < clientes.size(); i++) {
+            if (clientes.get(i).getId().equals(id)) {
+                selecionado = clientes.get(i);
+                break;
+            }
+        }
+        if (selecionado != null)
+            clientes.remove(selecionado);
+    }
+}
